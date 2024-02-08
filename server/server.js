701163a812c6023012016d0c24237ae348624c42
goodluck <goodluckkumar04@gmail.com>
2024-02-08 19:13:36 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(helmet());
 // cross origin access
 app.use(cors({
-    origin: "https://urlener-r8ezbfhmc-kumarhukum01-gmailcom.vercel.app/", // Update with your frontend's URL
+    origin: process.env.CLIENT_URL, // Update with your frontend's URL
     credentials: true
   }));
 // parsing cookies
