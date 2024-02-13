6abbc0f7a34b320c601d2d2c993fa947df391ba3
goodluck <goodluckkumar04@gmail.com>
2024-02-13 15:38:20 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(helmet());
 // cross origin access
 app.use(cors({
-    origin: ['https://urlener.vercel.app/'],
+    origin: 'https://urlener.vercel.app',
     credentials: true,
 }));
 // parsing cookies
